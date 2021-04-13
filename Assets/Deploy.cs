46e7d13bd695da2be49ec1bd701002d1caf9bbdf
Brooks, Dezmond G <brook340@msu.edu>
2021-04-13 19:56:11 -0400

    did stuff
    did stuff

---
+++ Assets/Deploy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deploy : MonoBehaviour
+{
+    public GameObject CubeSat;
+    public Transform Earth;
+    public Transform Planet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject a = Instantiate(CubeSat) as GameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(Earth.position, Planet.position);
+        UnityEngine.Debug.Log("Distance Between: " + dist);
+    }
+
+    private float DistanceBetween(Transform item1, Transform item2)
+    {
+        //return Vector3.Distance(item1.position, item2.posistion);
+        return 0;
+    }
+}
